fcac4bd8fa650933d1589fe672c84760a1f76eed
Adam Karr <adamlkarr@gmail.com>
2018-04-05 17:34:40 -0500

    Managed to get errors to work

--- main.py
+++ main.py
@@ -4,9 +4,36 @@ import os
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def index():
-    sign_in = request.form('sign-in.html')
-    return render_template('sign-in.html')
+    encoded_error = request.args.get("error")
+    #username = str(request.form.get('username'))
+    #if len(username) < 3:
+    #    error = "That username is too short"
+    #    return redirect("/?error=" + error)
+    #if len(username) > 20:
+    #    error = "That username is too long"
+    #    return redirect("/?error=" + error)
+    #password = request.form.get('pword')
+    #verified_password = request.form.get('verpword')
+    #email = request.form.get('email')
+    return render_template('sign-in.html', error=encoded_error)
 
+@app.route("/welcome.html", methods=['POST'])
+def welcome():
+    username = str(request.form.get('username'))
+    for char in username:
+        if char == '':
+            error = "Username cannot contain spaces."
+            return redirect("/?error=" + error)            )
+    if len(username) < 3:
+        error = "That username is too short"
+        return redirect("/?error=" + error)
+    if len(username) > 20:
+        error = "That username is too long"
+        return redirect("/?error=" + error)
+    password = request.form.get('pword')
+    verified_password = request.form.get('verpword')
+    email = request.form.get('email')
+    return render_template('welcome.html')
 app.run()
