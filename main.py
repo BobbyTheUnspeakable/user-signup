cfd81d79336a98d719a02a4e004633e2527c4297
Adam Karr <adamlkarr@gmail.com>
2018-04-05 19:58:04 -0500

    Username and Password validation done

--- main.py
+++ main.py
@@ -22,18 +22,28 @@ def index():
 @app.route("/welcome.html", methods=['POST'])
 def welcome():
     username = str(request.form.get('username'))
-    for char in username:
-        if char == '':
-            error = "Username cannot contain spaces."
-            return redirect("/?error=" + error)            )
+    password = str(request.form.get('pword'))
+    verified_password = str(request.form.get('verpword'))
+    email = str(request.form.get('email'))
+
+    if username.isalnum() == False:
+        error = "Username cannot contain spaces."
+        return redirect("/?error=" + error)
     if len(username) < 3:
         error = "That username is too short"
         return redirect("/?error=" + error)
     if len(username) > 20:
         error = "That username is too long"
         return redirect("/?error=" + error)
-    password = request.form.get('pword')
-    verified_password = request.form.get('verpword')
-    email = request.form.get('email')
+    if password.isalnum() == False:
+        error = "Password cannot contain spaces."
+        return redirect("/?error=" + error)
+    if len(password) < 3:
+        error = "That password is too short"
+        return redirect("/?error=" + error)
+    if len(password) > 20:
+        error = "That password is too long"
+        return redirect("/?error=" + error)
+        
     return render_template('welcome.html')
 app.run()
