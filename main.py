f4a0764c426b578db44bafaa1a096604de59e7c8
Adam Karr <adamlkarr@gmail.com>
2018-04-07 00:58:17 -0500

    Validation finished

--- main.py
+++ main.py
@@ -7,16 +7,7 @@ app.config['DEBUG'] = True
 @app.route("/", methods=["POST", "GET"])
 def index():
     encoded_error = request.args.get("error")
-    #username = str(request.form.get('username'))
-    #if len(username) < 3:
-    #    error = "That username is too short"
-    #    return redirect("/?error=" + error)
-    #if len(username) > 20:
-    #    error = "That username is too long"
-    #    return redirect("/?error=" + error)
-    #password = request.form.get('pword')
-    #verified_password = request.form.get('verpword')
-    #email = request.form.get('email')
+    
     return render_template('sign-in.html', error=encoded_error)
 
 @app.route("/welcome.html", methods=['POST'])
@@ -25,25 +16,39 @@ def welcome():
     password = str(request.form.get('pword'))
     verified_password = str(request.form.get('verpword'))
     email = str(request.form.get('email'))
+    list_email = list(email)
 
     if username.isalnum() == False:
-        error = "Username cannot contain spaces."
-        return redirect("/?error=" + error)
+        user_error = "Username cannot contain spaces."
+        return redirect("/?error=" + user_error)
     if len(username) < 3:
-        error = "That username is too short"
-        return redirect("/?error=" + error)
+        user_error = "That username is too short"
+        return redirect("/?error=" + user_error)
     if len(username) > 20:
-        error = "That username is too long"
-        return redirect("/?error=" + error)
+        user_error = "That username is too long"
+        return redirect("/?error=" + user_error)
     if password.isalnum() == False:
-        error = "Password cannot contain spaces."
-        return redirect("/?error=" + error)
+        pw_error = "Password cannot contain spaces."
+        return redirect("/?error=" + pw_error)
     if len(password) < 3:
-        error = "That password is too short"
-        return redirect("/?error=" + error)
+        pw_error = "That password is too short"
+        return redirect("/?error=" + pw_error)
     if len(password) > 20:
-        error = "That password is too long"
-        return redirect("/?error=" + error)
-        
-    return render_template('welcome.html')
+        pw_error = "That password is too long"
+        return redirect("/?error=" + pw_error)
+    if verified_password != password:
+        vpw_error = "Passwords do not match"
+        return redirect("/?error=" + vpw_error)
+    if email != '':
+        if len(email) < 3:
+            email_error = "That email is too short"
+            return redirect("/?error=" + email_error)
+        if len(email) > 20:
+            email_error = "That email is too long"
+            return redirect("/?error=" + email_error)
+        if ('@' not in list_email) == True and ('.' not in list_email) == True:
+            email_error = "Not a valid email"
+            return redirect('/?error=' + email_error)
+
+    return render_template('welcome.html', username = username)
 app.run()
