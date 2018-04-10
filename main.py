8d21037bcee5db4f3b8f39bade9771856d1f1dc4
Adam Karr <adamlkarr@gmail.com>
2018-04-09 19:24:11 -0500

    ITS ALIVEgit add *!

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ def validate(string):
 
 @app.route("/", methods=["POST", "GET"])
 def index():
-    
     return render_template('sign-in.html') 
 
 @app.route("/welcome.html", methods=['POST', "GET"])
@@ -25,31 +24,30 @@ def welcome():
 
     if validate(username) == False:
         user_error = "Usernames should be between 3 and 20 characters"
-        return redirect("/?error=" + user_error)
+        return render_template("sign-in.html", user_error = user_error)
     if username.isalnum() == False:
         user_error = "Username cannot contain spaces."
-        return redirect("/?error=" + user_error)
+        return render_template("sign-in.html", user_error = user_error)
 
     if validate(password) == False:
         pw_error = "Passwords should be between 3 and 20 characters"
-        return redirect("/?error=" + pw_error)
+        return render_template("sign-in.html", pw_error = pw_error, username = username, email=email)
     if password.isalnum() == False:
         pw_error = "Password cannot contain spaces."
-        return redirect("/?error=" + pw_error)
+        return render_template("sign-in.html", pw_error = pw_error, username = username, email=email)
 
     if verified_password != password:
         vpw_error = "Passwords do not match"
-        return redirect("/?error=" + vpw_error)
+        return render_template("sign-in.html", vpw_error = vpw_error, username = username, email=email)
 
     if email != '':
         if validate(email) == False:
             email_error = "emails should be between 3 and 20 characters"
-            return redirect('/?error=' + email_error)
+            return render_template("sign-in.html", email_error = email_error)
         if ('@' not in list_email) == True and ('.' not in list_email) == True:
             email_error = "Not a valid email"
-            return redirect('/?error=' + email_error)
+            return render_template("sign-in.html", email_error = email_error)
 
-    return render_template('welcome.html', username = username)
-    #user_error=user_error, pw_error=pw_error, vpw_error=vpw_error,
-     #email_error=email_error)
+    return render_template('welcome.html', username = username, email=email)
+
 app.run()
